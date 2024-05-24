61678dbeebe6c4e42ebd250239038e729ce0775f
Pan Zeng <u7799442@anu.edu.au>
2024-05-24 23:53:56 +1000

    project

--- src/main.cpp
+++ src/main.cpp
@@ -106,6 +106,8 @@ int main(int argc, const char * argv[]) {
     glPointSize(3);
     
     /** Redering loop **/
+    int count = 0;
+    auto start = std::chrono::high_resolution_clock::now();
     while (!glfwWindowShouldClose(window)) {
         /** Set background clolor **/
         glClearColor(bgColor.x, bgColor.y, bgColor.z, 1.0); // Set color value (R,G,B,A) - Set Status
@@ -132,6 +134,12 @@ int main(int argc, const char * argv[]) {
             } else {
                 cloth.step(constraint, currentRigidType, obj, TIME_STEP);
             }
+        }
+        count++;
+        if (count == 100) {
+            auto end = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            std::cout << duration.count() << std::endl;
         }
         cloth.compute_normal();
 
